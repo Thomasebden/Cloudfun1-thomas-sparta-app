7fb1475c0c6fec8b57683bfaa5536dca4db38f6c
Thomasebden <tebden.te@gmail.com>
2024-12-20 12:10:29 +0000

    final final

--- scripts/database.sh
+++ scripts/database.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-
 sudo apt update -y
-
 
 sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
 
-
 sudo DEBIAN_FRONTEND=noninteractive apt-get install gnupg curl
-
 
 curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
    sudo gpg --yes -o /usr/share/keyrings/mongodb-server-7.0.gpg \
    --dearmor
 
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-
 
 sudo apt-get update
 
